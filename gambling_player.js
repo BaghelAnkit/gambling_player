b8781278901c211452f72299c1cd98442c053ddc
anubhav singh <anubhavsingh230@gmail.com>
2023-03-10 02:55:02 +0530

    money_calculation

--- gambling_player.js
+++ gambling_player.js
@@ -1,8 +1,11 @@
+let totalmoney = 100;
 let temp = Math.round(Math.random())
 if (temp == 0) {
     console.log("player win", temp)
+    totalmoney++
 }
 if (temp == 1) {
     console.log("player loss", temp)
-
+    totalmoney--
 }
+console.log("totalmoney", totalmoney)
