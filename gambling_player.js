db9b5bce6bdf4a9c8677dbdee7cbe84d809aa2ca
anubhav singh <anubhavsingh230@gmail.com>
2023-03-10 03:47:41 +0530

    totalchance_final

--- gambling_player.js
+++ gambling_player.js
@@ -1,13 +1,24 @@
 let totalmoney = 100;
+let wincounter = 0,
+    losscounter = 0,
+    totalchance = 0;
 while (totalmoney < 200 && totalmoney > 0) {
+    totalchance++
     let temp = Math.round(Math.random())
     if (temp == 0) {
         console.log("player win", temp)
         totalmoney++
+        wincounter++
     }
     if (temp == 1) {
         console.log("player loss", temp)
         totalmoney--
+        losscounter++
     }
 }
+let totalchance2 = wincounter + losscounter
 console.log("totalmoney", totalmoney)
+console.log("wincounter", wincounter)
+console.log("losscounter", losscounter)
+console.log("totalchance", totalchance)
+console.log("totalchance2", totalchance2)
