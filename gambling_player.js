1043d9918351391f60030ed2d706dc81d19f7418
anubhav singh <anubhavsingh230@gmail.com>
2023-03-10 03:27:35 +0530

    money_200_or_0

--- gambling_player.js
+++ gambling_player.js
@@ -1,11 +1,13 @@
 let totalmoney = 100;
-let temp = Math.round(Math.random())
-if (temp == 0) {
-    console.log("player win", temp)
-    totalmoney++
-}
-if (temp == 1) {
-    console.log("player loss", temp)
-    totalmoney--
+while (totalmoney < 200 && totalmoney > 0) {
+    let temp = Math.round(Math.random())
+    if (temp == 0) {
+        console.log("player win", temp)
+        totalmoney++
+    }
+    if (temp == 1) {
+        console.log("player loss", temp)
+        totalmoney--
+    }
 }
 console.log("totalmoney", totalmoney)
